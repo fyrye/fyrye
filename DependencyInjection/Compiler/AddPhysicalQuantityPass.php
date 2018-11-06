823614905aeb7172630c77149f4a8c8526c554f8
fyrye <fyrye@users.noreply.github.com>
2018-11-06 01:37:11 -0500

    3.x (#2)
    * fix units as boolean
    
    * fix documentation of getConfiguration
    
    * optimized
    
    * updated to Symfony 3.4|4.0
    
    * initial commit of v3.0.0

--- DependencyInjection/Compiler/AddPhysicalQuantityPass.php
+++ DependencyInjection/Compiler/AddPhysicalQuantityPass.php
@@ -4,23 +4,23 @@
  * @author fyrye <admin@fyrye.com>
  * @version 2017.01.15
  */
+
 namespace Fyrye\Bundle\PhpUnitsOfMeasureBundle\DependencyInjection\Compiler;
 
+use Fyrye\Bundle\PhpUnitsOfMeasureBundle\DependencyInjection\UnitNamingTrait;
+use Fyrye\Bundle\PhpUnitsOfMeasureBundle\Registry\Manager;
+use Fyrye\Bundle\PhpUnitsOfMeasureBundle\Registry\QuantityDefinition;
 use PhpUnitsOfMeasure\AbstractPhysicalQuantity;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\Finder\Finder;
 
 /**
  * {@inheritDoc}
  */
 class AddPhysicalQuantityPass implements CompilerPassInterface
 {
-    /**
-     * @var array
-     */
-    private $services = [];
+    use UnitNamingTrait;
 
     /**
      * You can modify the container here before it is dumped to PHP code.
@@ -28,71 +28,27 @@ class AddPhysicalQuantityPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        if (
-            !$container->hasDefinition('php_units_of_measure.registry_manager') ||
-            !class_exists(AbstractPhysicalQuantity::class) ||
-            !in_array($container->getParameter('php_units_of_measure.auto'), ['ALL', 'BUNDLES'], true)
+        if (!$container->hasDefinition(Manager::class) ||
+            !class_exists(AbstractPhysicalQuantity::class)
         ) {
             return;
         }
-        if (!class_exists(Finder::class)) {
-            throw new \RuntimeException(
-                'You need the symfony/finder component to register PhysicalQuantity objects from bundles.'
-            );
-        }
-        /** @var array|null $bundles */
-        if ($bundles = $container->getParameter('php_units_of_measure.bundles')) {
-            $bundleMetaData = $container->getParameter('kernel.bundles_metadata');
-            $finder = new Finder();
-            foreach ($bundles as $bundle) {
-                if (!isset($bundleMetaData[$bundle]['path'])) {
-                    continue;
-                }
-                $bundlePath = $bundleMetaData[$bundle]['path'];
-                $physicalQuantityPath = $bundlePath . '/PhysicalQuantity';
-                if (!is_dir($physicalQuantityPath)) {
-                    continue;
-                }
-                $finder->files()->name('*.php')->in($physicalQuantityPath);
-                $this->scanBundle($container, $finder, $bundleMetaData[$bundle]['namespace']);
-            }
-            foreach ($this->services as $serviceName) {
-                $container
-                    ->getDefinition('php_units_of_measure.registry_manager')
-                    ->addMethodCall('registerDefinition', [new Reference($serviceName)]);
+        if ($services = $container->findTaggedServiceIds('php_units_of_measure.extension', true)) {
+            $managerDefinition = $container->getDefinition(Manager::class);
+            foreach ($services as $id => $tags) {
+                //retrieve the tagged service definition and remove it
+                $serviceDefinition = $container->getDefinition($id);
+                $container->removeDefinition($id);
+                //create a QuantityDefinition to proxy the tagged service
+                $class = $serviceDefinition->getClass();
+                $name = basename($class);
+                $container->register($class, QuantityDefinition::class)
+                    ->setArguments([$name, $class])
+                    ->setPublic(true);
+                $container->setAlias('php_units_of_measure.quantity.' . $this->normalizeName($name), $class)
+                    ->setPublic(true);
+                $managerDefinition->addMethodCall('registerDefinition', [new Reference($class)]);
             }
         }
-    }
-
-    /**
-     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
-     * @param \Symfony\Component\Finder\Finder $finder
-     * @param $namespace
-     */
-    private function scanBundle(ContainerBuilder $container, Finder $finder, $namespace)
-    {
-        foreach ($finder as $file) {
-            $quantityName = $file->getBasename('.php');
-            $class = $namespace . '\\PhysicalQuantity\\' . $quantityName;
-            $serviceName = 'php_units_of_measure.quantity.' . $this->normalizeName($quantityName);
-            $r = new \ReflectionClass($class);
-            if ($r->isSubclassOf(AbstractPhysicalQuantity::class) && !$r->isAbstract()) {
-                if ($container->has($serviceName)) {
-                    $container->removeDefinition($serviceName);
-                }
-                $service = $container->register($serviceName, '%php_units_of_measure.quantity_definition.class%');
-                $service->setArguments([$quantityName, $class,]);
-                $this->services[] = $serviceName;
-            }
-        }
-    }
-
-    /**
-     * @param string $name
-     * @return string
-     */
-    private function normalizeName($name)
-    {
-        return preg_replace('[^a-z0-9_]', '', strtolower($name));
     }
 }
---
+++ DependencyInjection/UnitNamingTrait.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * UnitNamingTrait.php
+ * @author fyrye <admin@fyrye.com>
+ * @version 2018.11.05
+ */
+
+namespace Fyrye\Bundle\PhpUnitsOfMeasureBundle\DependencyInjection;
+
+/**
+ * Trait UnitNamingTrait
+ * @package Fyrye\Bundle\PhpUnitsOfMeasureBundle\DependencyInjection
+ */
+trait UnitNamingTrait
+{
+    /**
+     * @param string $name
+     * @return string
+     */
+    private function normalizeName($name)
+    {
+        return preg_replace('/[^a-z0-9_]/', '', strtolower($name));
+    }
+}
