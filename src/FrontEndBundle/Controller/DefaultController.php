eb5e0e4d27a653ffa417ee8250475019bd6bc98f
fyrye <fyrye@users.noreply.github.com>
2018-11-11 22:08:42 -0500

    fixed sensiolabs insight issues
    fix #5
    fix #7
    fix #8
    fix #9

--- src/FrontEndBundle/Controller/DefaultController.php
+++ src/FrontEndBundle/Controller/DefaultController.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace FrontEndBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Fyrye\Bundle\PhpUnitsOfMeasureBundle\Registry\Manager;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
@@ -10,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", methods={"GET", "POST"})
      */
     public function indexAction()
     {
@@ -18,32 +20,46 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/uom", name="unit_of_measure")
+     * @Route("/uom/{quantity}/{amount}/{from_unit}/{to_unit}",
+     *     name="unit_of_measure",
+     *     methods={"GET", "POST"},
+     *     requirements={
+     *          "quantity": "\w+",
+     *          "amount": "\d+",
+     *          "from_unit": "\w+",
+     *          "to_unit": "\w+"
+     *      }
+     * )
+     * @param \Fyrye\Bundle\PhpUnitsOfMeasureBundle\Registry\Manager $manager
+     * @param string $quantity
+     * @param int $amount
+     * @param string $from_unit
+     * @param string $to_unit
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function unitOfMeasureAction()
+    public function unitOfMeasureAction(Manager $manager, $quantity, $amount, $from_unit, $to_unit)
     {
-        $quantity = $this->container->get('php_units_of_measure.quantity');
-        $mile = $quantity->getUnit('length', 1, 'mile');
+
+        $unit = $manager->getUnit($quantity, $amount, $from_unit);
+        $pq = $this->container->get('php_units_of_measure.quantity');
+        $mile = $pq->getUnit('length', 1, 'mile');
         $length = $this->container->get('php_units_of_measure.quantity.length');
         $yard = $length->getUnit(1, 'yd');
-        $another = $length->getUnit(2, 'yd');
-        $pascal = $quantity->getUnit('pressure', '1', 'pascal');
+        $pascal = $pq->getUnit('pressure', 1, 'pascal');
         $viscosity = $this->container->get('php_units_of_measure.quantity.viscosity');
         $stokes = $viscosity->getUnit(1, 'stoke');
         $um = $length->getUnit(1, 'um');
 
-        return $this->render(
-            'FrontEndBundle:Default:uom.html.twig',
-            [
-                'feet' => $yard->toUnit('ft'),
-                'inches' => $yard->toUnit('in'),
-                'feet_2' => $another->toUnit('ft'),
-                'inches_2' => $another->toUnit('in'),
-                'mile_feet' => $mile->toUnit('ft'),
-                'psi' => $pascal->toUnit('psi'),
-                'cst' => $stokes->toUnit('cst'),
-                'm' => $um->toUnit('m'),
+        return $this->render('FrontEndBundle:Default:uom.html.twig', [
+                'units' => [
+                    ['from' => $from_unit, 'to' => $to_unit, 'amount' => $unit->toUnit($to_unit)],
+                    ['from' => 'yard', 'to' => 'feet', 'amount' => $yard->toUnit('ft')],
+                    ['from' => 'yard', 'to' => 'inch', 'amount' => $yard->toUnit('in')],
+                    ['from' => 'mile', 'to' => 'feet', 'amount' => $mile->toUnit('ft')],
+                    ['from' => 'pascal', 'to' => 'psi', 'amount' => $pascal->toUnit('psi')],
+                    ['from' => 'stokes', 'to' => 'cst', 'amount' => $stokes->toUnit('cst')],
+                    ['from' => 'um', 'to' => 'm', 'amount' => $um->toUnit('m')],
+                ],
             ]
         );
     }
